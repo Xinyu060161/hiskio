eed4aadc77b7d7fbe49bc31f3ebc3ee9a9ae8fe9
Xinyu060161 <q602363633@gmail.com>
2024-12-21 23:49:52 +0800

    改資庫名稱

--- 3-Note-app/beginning-files/connection.php
+++ 3-Note-app/beginning-files/connection.php
@@ -10,7 +10,7 @@ class connection
         echo "test";
         // 假設這裡創建了一個 PDO 實例
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=HISKio_test2024', 'username', 'password');
+            $this->pdo = new PDO('mysql:host=localhost;dbname=HISKio_test2024', '', 'newpassword');
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
