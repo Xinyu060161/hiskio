043dfa145616f538b2ca3f3099b2848055723d7d
Xinyu060161 <q602363633@gmail.com>
2025-01-06 23:31:08 +0800

    nall

--- 3-Note-app/beginning-files/connection.php
+++ 3-Note-app/beginning-files/connection.php
@@ -22,7 +22,7 @@ class connection
         $sql     = "SELECT * FROM SQL_test ORDER BY $column DESC";
         $stmt    = $this->pdo->query($sql);
         $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
-        return $results
+        return $results;
     }
 
 }
