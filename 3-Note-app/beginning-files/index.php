433a6a86b9d00592853bbd6cf27e6a5447384f44
Xinyu060161 <q602363633@gmail.com>
2025-01-14 23:14:26 +0800

    add note

--- 3-Note-app/beginning-files/index.php
+++ 3-Note-app/beginning-files/index.php
@@ -68,6 +68,24 @@ echo '<pre>',print_r($notes),'<pre>';
             </form>
         </div>
     </div>
+
+    <div class="notes">
+        <?php foreach($notes as $note): ?>
+        <div class="note">
+            <div class="title">
+                <a href="?id=<?php echo $note['id']; ?>"><?php echo $note['title']; ?></a>
+            </div>
+            <div class="description">
+              <?php echo $note['description']; ?>
+            </div>
+            <small><?php echo $note['created_date']; ?></small>
+            <form action="delete.php" method="post">
+              <input type="hidden" name="id" value="<?php echo $note['id']; ?>">
+            <button class="close">X</button>
+            </form>
+        </div>
+        <?php endforeach; ?>
+
   </div>
 </body>
 </html>
