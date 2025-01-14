ee8c0c6565ea4aeb43fb36fc8ecb6178e03420a9
Xinyu060161 <q602363633@gmail.com>
2025-01-15 02:06:39 +0800

    add note

--- 3-Note-app/beginning-files/index.php
+++ 3-Note-app/beginning-files/index.php
@@ -34,7 +34,7 @@ print_r($notes);
       <?foreach($notes as $T1): ?>
         <div class="note">
             <div class="title">
-            <a href="?id=<?php echo $T1['id']; ?>"><?php echo $T1['title']; ?></a>
+            <a href="#"<?php echo $T1['title']; ?></a>
             </div>
             <div class="description">
               <? echo $T1['description'];?>
