d448c88bb7a039b65ca389f3978e36744d3c9e97
Xinyu060161 <q602363633@gmail.com>
2025-01-15 02:49:42 +0800

    add note

--- 3-Note-app/beginning-files/index.php
+++ 3-Note-app/beginning-files/index.php
@@ -48,17 +48,17 @@ print_r($notes);
           <? endforeach; ?>  
 
     <div class="notes">
-        <?php foreach($notes as $a): ?>
+        <?php foreach($notes as $T1): ?>
         <div class="note">
             <div class="title">
-                <a href="?id=<?php echo $a['id']; ?>"><?php echo $a['title']; ?></a>
+                <a href="?id=<?php echo $T1['id']; ?>"><?php echo $T1['title']; ?></a>
             </div>
             <div class="description">
-              <?php echo $a['description']; ?>
+              <?php echo $T1['description']; ?>
             </div>
-            <small><?php echo $a['created_date']; ?></small>
+            <small><?php echo $T1['created_date']; ?></small>
             <form action="delete.php" method="post">
-              <input type="hidden" name="id" value="<?php echo $a['id']; ?>">
+              <input type="hidden" name="id" value="<?php echo $T1['id']; ?>">
             <button class="close">X</button>
             </form>
         </div>
