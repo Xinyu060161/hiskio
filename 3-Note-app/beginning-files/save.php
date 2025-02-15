1235b9cbb0f611a6d7a6e2079c58db02e319c23d
Xinyu060161 <q602363633@gmail.com>
2025-02-16 01:23:51 +0800

    更新 save.php 檔案

---
+++ 3-Note-app/beginning-files/save.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "this is PHP";
+
+// // echo '<pre>',print_r($_POST),'</pre>';
+
+// $connection = require_once './Connection.php';
+
+// $id = $_POST['id'] ?? "";
+
+// if($id) {
+//   $connection->updateNote($id, $_POST);
+// } else {
+//   $connection->addNote($_POST);
+// }
+
+// header('Location: index.php');
